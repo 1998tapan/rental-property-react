7ad2d09c60f093996eba643ef0b9ac16fefa2087
Tapan Dabrai <1998tapan@gmail.com>
2024-07-13 16:32:52 +0530

    styles, views refactored, landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,22 @@ import Rental from './Rental';
 
 function App() {
   const properties = [
-    { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-    { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-    { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-    { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-    { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-    { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+    { id: 129031, name: "Desert Yurt", rating: 4.9, price: "15,000" },
+    { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: "25,000" },
+    { id: 129032, name: "Cactus Retreat", rating: 4.75, price: "30,000" },
+    { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 1200 },
+    { id: 129034, name: "Oceanview Condo", rating: 4.7, price: "14,000" },
+    { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: "960" },
+    { id: 129036, name: "Palm Springs", rating: 4.6, price: "11,000" },
+    { id: 129037, name: "Beachfront View", rating: 4.44, price: "13,600" },
   ];
 
   return (
     <>
-      <Rental />
-      <Rental />
-      <Rental />
-      <Rental />
+      {properties.map(
+        rental => (
+          <Rental key={rental.id} {...rental} />
+        ))}
     </>
   )
 }
--- src/Rental.jsx
+++ src/Rental.jsx
@@ -1,6 +1,6 @@
 import "./Rental.css";
 
-export default function Rental(properties) {
+export default function Rental({ name, price, rating }) {
     const url = `https://picsum.photos/400?random=${Math.random()}`;
 
     return (
@@ -9,7 +9,16 @@ export default function Rental(properties) {
                 <img src={url} alt="" />
             </div>
             <div className="rental-info">
-                <p>Name: Demo</p>
+                <div className="rental-main-text">
+                    <div className="rental-title">{name}</div>
+                    <div className="rating">{rating}&#9733;</div>
+                </div>
+                <div className="rental-duration">
+                    9-21 Oct
+                </div>
+                <div className="rental-price">
+                    <span style={{ fontWeight: 600 }}>&#8377;{price}</span> night
+                </div>
             </div>
         </div>
     )
